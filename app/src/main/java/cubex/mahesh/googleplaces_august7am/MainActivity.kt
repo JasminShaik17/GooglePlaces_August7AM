3a7dcaa27e039e907d90e20414bea093e548ae3b
maheshthippala <mahesh.cubex@gmail.com>
2018-08-19 12:57:06 +0530

    Added Maps functionality...

--- app/src/main/java/cubex/mahesh/googleplaces_august7am/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/googleplaces_august7am/MainActivity.kt
@@ -7,17 +7,24 @@ import android.location.LocationListener
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
 import android.widget.Toast
 import com.google.android.gms.location.places.ui.PlacePicker
 import cubex.mahesh.googleplaces_august7am.beans.PlacesBean
 import cubex.mahesh.googleplaces_august7am.beans.ResultsItem
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.indi_view.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
+var list_new :List<ResultsItem>? = null
 
 class MainActivity : AppCompatActivity() {
         var lati:Double? = null
@@ -68,15 +75,52 @@ class MainActivity : AppCompatActivity() {
                                         response: Response<PlacesBean>?) {
                     var bean = response!!.body()
                     var list = bean!!.results
-                    var temp_list = mutableListOf<String>()
+                    show_all.setOnClickListener {
+                        var i = Intent(this@MainActivity,
+                                MapsActivity::class.java )
+                        i.putExtra("from_showall",true)
+                        list_new = list
+                        startActivity(i)
+                    }
+                 /*   var temp_list = mutableListOf<String>()
                     for(item in list!!){
                         temp_list.add(item.name+"\n"+
                                         item.vicinity)
                     }
                     var adapter = ArrayAdapter<String>(this@MainActivity,
                             android.R.layout.simple_list_item_single_choice,
-                            temp_list)
-                    lview.adapter = adapter
+                            temp_list) */
+                    lview.adapter = object:BaseAdapter(){
+                        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+                            var inflater = LayoutInflater.from(this@MainActivity)
+                            var view = inflater.inflate(R.layout.indi_view,null)
+                            view.name.text = list!!.get(p0).name
+                            view.address.text = list!!.get(p0).vicinity
+                            view.b1.setOnClickListener {
+
+                                var i = Intent(this@MainActivity,
+                                                        MapsActivity::class.java)
+                                i.putExtra("latitude",list.get(p0).
+                                        geometry.location.lat)
+                                i.putExtra("longitude",list.get(p0).
+                                        geometry.location.lng)
+                                i.putExtra("from_showall",false)
+                                startActivity(i)
+
+                            }
+                            return view
+                        }
+                        override fun getItem(p0: Int): Any {
+                            return  0;
+                          }
+                        override fun getItemId(p0: Int): Long {
+                            return 0;
+                        }
+                        override fun getCount(): Int {
+                            return  list!!.size
+                        }
+
+                    }
                 }
 
                 override fun onFailure(call: Call<PlacesBean>?, t: Throwable?) {
